072765dc577f4c5a2898fb753885ef3eb009bfff
sasso0101 <salvatore.andaloro01@gmail.com>
2020-11-02 13:31:04 +0100

    Added debug prints

--- updater.py
+++ updater.py
@@ -14,8 +14,10 @@ if somethingChanged(nationalData) or force:
     print('Something changed. Updating...')
     yesterday = datetime.today() - timedelta(days=1)
     yesterday = yesterday.strftime('%Y%m%d')
+    print(yesterday)
     regionsData = getData('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv')
     regionsDataYesterday = getData('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-'+yesterday+'.csv')
+    print(regionsDataYesterday)
     daysAgo30 = datetime.today() - timedelta(days=30)
     daysAgo30 = daysAgo30.strftime('%Y%m%d')
     provincesData = getData('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-latest.csv')
@@ -41,15 +43,15 @@ if somethingChanged(nationalData) or force:
     data['id'] = time.time()
     uploadData(json.dumps(data))
 
-    if not notificationSentToday():
-        pushTitle = "Nuovi casi coronavirus - Aggiornamento ore 17:00"
-        pushMessage = "Dati aggiornati con le informazioni rilasciate dalla Protezione civile alle 17:00."
-        sendPushNotification(pushTitle, pushMessage)
+    # if not notificationSentToday():
+    #     pushTitle = "Nuovi casi coronavirus - Aggiornamento ore 17:00"
+    #     pushMessage = "Dati aggiornati con le informazioni rilasciate dalla Protezione civile alle 17:00."
+    #     sendPushNotification(pushTitle, pushMessage)
 
-        telegramMessage = '<b>Bollettino coronavirus - {}</b>\r\n🟧 {:n} contagi oggi ({:n} attualmente positivi)\r\n🟥 {:n} decessi ({:n} decessi totali)\r\n🟩 {:n} guariti oggi ({:n} guariti totali)\r\n⬜️ {:n} tamponi effettuati oggi ({:n} tamponi totali)\r\n\r\n{:n} in terapia intensiva || {:n} ospedalizzati\r\nVedi i casi nella tua regione su <a href=\"https://coronaviruslive.it\">coronaviruslive.it</a>'.format(now.strftime("%d/%m/%Y"), data['today']['newActiveCases'], data['today']['activeCases'], data['today']['newDeaths'], data['today']['deaths'], data['today']['newRecovered'], data['today']['recovered'], data['today']['newTests'], data['today']['tests'], data['today']['intensiveCare'], data['today']['hospitalized'])
-        sendTelegramMessage(telegramMessage)
+    #     telegramMessage = '<b>Bollettino coronavirus - {}</b>\r\n🟧 {:n} contagi oggi ({:n} attualmente positivi)\r\n🟥 {:n} decessi ({:n} decessi totali)\r\n🟩 {:n} guariti oggi ({:n} guariti totali)\r\n⬜️ {:n} tamponi effettuati oggi ({:n} tamponi totali)\r\n\r\n{:n} in terapia intensiva || {:n} ospedalizzati\r\nVedi i casi nella tua regione su <a href=\"https://coronaviruslive.it\">coronaviruslive.it</a>'.format(now.strftime("%d/%m/%Y"), data['today']['newActiveCases'], data['today']['activeCases'], data['today']['newDeaths'], data['today']['deaths'], data['today']['newRecovered'], data['today']['recovered'], data['today']['newTests'], data['today']['tests'], data['today']['intensiveCare'], data['today']['hospitalized'])
+    #     sendTelegramMessage(telegramMessage)
 
-        facebookMessage = 'Bollettino coronavirus - {}\r\n🟧 {:n} contagi oggi ({:n} attualmente positivi)\r\n🟥 {:n} decessi ({:n} decessi totali)\r\n🟩 {:n} guariti oggi ({:n} guariti totali)\r\n⬜️ {:n} tamponi effettuati oggi ({:n} tamponi totali)\r\n\r\n{:n} in terapia intensiva || {:n} ospedalizzati\r\nVedi i casi nella tua regione su https://coronaviruslive.it'.format(now.strftime("%d/%m/%Y"), data['today']['newActiveCases'], data['today']['activeCases'], data['today']['newDeaths'], data['today']['deaths'], data['today']['newRecovered'], data['today']['recovered'], data['today']['newTests'], data['today']['tests'], data['today']['intensiveCare'], data['today']['hospitalized'])
-        sendFacebookMessage(facebookMessage)
+    #     facebookMessage = 'Bollettino coronavirus - {}\r\n🟧 {:n} contagi oggi ({:n} attualmente positivi)\r\n🟥 {:n} decessi ({:n} decessi totali)\r\n🟩 {:n} guariti oggi ({:n} guariti totali)\r\n⬜️ {:n} tamponi effettuati oggi ({:n} tamponi totali)\r\n\r\n{:n} in terapia intensiva || {:n} ospedalizzati\r\nVedi i casi nella tua regione su https://coronaviruslive.it'.format(now.strftime("%d/%m/%Y"), data['today']['newActiveCases'], data['today']['activeCases'], data['today']['newDeaths'], data['today']['deaths'], data['today']['newRecovered'], data['today']['recovered'], data['today']['newTests'], data['today']['tests'], data['today']['intensiveCare'], data['today']['hospitalized'])
+    #     sendFacebookMessage(facebookMessage)
 else:
     print("Nothing new.")
